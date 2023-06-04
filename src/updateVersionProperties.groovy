e17d03dd8922a270dd755cca4f10e0b96a80f6af
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-04 23:39:01 +0200

    add v prefix to version

--- src/updateVersionProperties.groovy
+++ src/updateVersionProperties.groovy
@@ -9,9 +9,13 @@ def versionPropertiesFile = me.parent.parent.resolve('version.properties')
 
 def version = LocalDate.now().format('Y.MM.dd')
 
+/** This is how the information is used:
+ * org.freeplane.main.mindmapmode.HttpVersionClient#parseProperties(java.net.URL, org.freeplane.core.util.FreeplaneVersion)
+ * org.freeplane.core.util.FreeplaneVersion#getVersion(java.lang.String) -- Ignores leading 'v' (e.g. "v1.2.6") and accept '.' and ' ' as separator.
+ */
 def template = """\
 version=${version}
-downloadUrl=https://github.com/macmarrum/freeplane-MapObfuscator/releases/download/v${version}/MapObfuscator-${version}.addon.mm
+downloadUrl=https://github.com/macmarrum/freeplane-MapObfuscator/releases/download/v${version}/MapObfuscator-v${version}.addon.mm
 freeplaneVersionFrom=1.8.0
 """
 println template
